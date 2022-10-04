0aedbbdd7f43f08e912714f3a7bc3d6400cb437d
andres <andrestecpile97@gmail.com>
2022-10-04 18:17:38 -0500

    Chore|Se ha comentado lo que se ha modificado

--- Ejercicio_4/src/com/generation/Codigo4.java
+++ Ejercicio_4/src/com/generation/Codigo4.java
@@ -6,17 +6,18 @@ public class Codigo4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in); 
+		Scanner s = new Scanner(System.in);//se agrego la libreria java.util.Scanner asi tambien se agrego System.in 
 	    
 	    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
 	    String j1 = s.nextLine();
 	    
 	    
 	    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
-	    //Scanner s2 = new Scanner(System.in);
+	    //Scanner s2 = new Scanner(System.in);//considero que esto esta de mas ya que funciona con el primer scan
 	    String j2 = s.nextLine();
 	    
-	    if ((j1.equals(j2))) {
+	    if ((j1.equals(j2))) {//a todos los comparativos if se les agrego la funcion .equals() ya que en este caso 
+	    	//la funcion .equals() retorna un true o false de acuerdo al caso
 	      System.out.println("Empate");
 	    } else { 
 	      int g = 2;
@@ -40,7 +41,7 @@ public class Codigo4 {
 	        	
 	      }
 	      System.out.println("Gana el jugador " + g);
-	    }
+	    }//se ordenaron y agregaron estas llaves pues habia unas que solo estaban abiertas pero no cerradas
 	  
 	}//llave de main
 	    } //llave de codigo
